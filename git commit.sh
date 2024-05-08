be25832c9147877b4825ce752f6c739ffd9c3cc0
dwb <donwiktorb@gmail.com>
2024-05-08 19:22:46 +0200

    .

---
+++ git commit.sh
@@ -0,0 +1,115 @@
+git commit
+
+git checkout -b bugFix
+
+git checkout -b bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
+
+git checkout C4
+git checkout bugFix^
+
+git branch -f bugFix C0
+git branch -f main C6
+git checkout C1
+
+
+git reset C1
+git checkout pushed
+git revert pushed
+
+git cherry-pick C3 C4 C7
+
+git rebase -i C1
+
+git rebase -i C1
+git checkout main
+git branch -f C4
+
+
+git rebase -i C1
+git commit --amend
+git rebase -i
+git branch -f main C3''
+
+git checkout main
+git branch -f main C1
+git commit --amend
+git cherry-pick C3
+
+
+git tag v1 C2
+git tag v0 C1
+git checkout v1
+
+git describe
+
+git commit
+
+ git branch -f main C2
+
+ git branch -f bugFix C1
+
+
+ git branch -f main C3
+
+ git rebase -i C2
+
+
+
+
+ git branch -f main C0
+
+
+
+ git branch -f bugFix C0
+
+
+ git branch -f main C4
+
+ git rebase -i C3'
+
+ git branch -f main C5
+
+ git rebase -i C4'
+
+ git branch -f main C6
+
+ git rebase -i C5'
+
+ git branch -f main C7
+
+ git rebase -i C7
+
+git checkout C2
+git branch bugWork
+git checkout main
+
+ git checkout C4
+
+ git rebase -i C1
+
+ git checkout C5
+
+ git rebase -i C1
+
+ git branch -f two C2''
+
+ git branch -f one C2'
+
+ git branch -f C2 three
+
+
+ git branch -f three C2
+
+ git checkout three
